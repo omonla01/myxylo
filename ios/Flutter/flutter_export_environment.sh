d975f123aeaa0b75d6ca3f69b19f7b4db8a76631
omonla01 <uthmanawwal@yahoo.com>
2022-06-12 12:35:34 +0100

    myxylophone

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\user\Downloads\xylophone-flutter-master (1)\xylophone-flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\user\Downloads\xylophone-flutter-master\xylophone-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+void playsound(int soundNumber) {
+  final player = AudioCache();
+                 player.play('note$soundNumber.wav');
+}
+    Expanded buildKey({Color color, int soundNumber}) {
+            return Expanded(
+                child: TextButton( 
+                  style: TextButton.styleFrom(
+                    backgroundColor:color,
+                  ),
+                   onPressed: () {
+                          playsound(soundNumber);       
+                }, child: null,
+                ),
+                );
+    }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column( 
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[ 
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.black, soundNumber: 2),
+              buildKey(color: Colors.blue, soundNumber: 3),
+              buildKey(color: Colors.yellow, soundNumber: 4),
+              buildKey(color: Colors.green, soundNumber: 5),
+              buildKey(color: Colors.pink, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ]
+            
+          ),
         ),
       ),
     );
